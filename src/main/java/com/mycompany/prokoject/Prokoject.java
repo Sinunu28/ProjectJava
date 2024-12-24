e5a7a62e55fcf350e2dabf0aa6b5cdededb1eaef
Sinunu28 <advinrabel2@gmail.com>
2024-12-24 22:09:04 +0700

    upload project

---
+++ src/main/java/com/mycompany/prokoject/Prokoject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prokoject;
+
+/**
+ *
+ * @author Advin Rabel
+ */
+public class Prokoject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
